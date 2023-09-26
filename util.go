b1a119d404a1cfebca74d3fa55c8e7201ffa6832
myyrakle <sssang97@naver.com>
2023-09-27 00:36:26 +0900

    add: Next methods

--- util.go
+++ util.go
@@ -44,13 +44,73 @@ func (this *Scanner) Next() string {
 	return string(buffer)
 }
 
-func (this *Scanner) NextInt() int {
+func (this *Scanner) NextByte() Byte {
 	rawData := this.Next()
 
 	if data, err := strconv.Atoi(rawData); err != nil {
 		panic(&InputMismatchException{message: rawData})
 	} else {
-		return data
+		return Byte(data)
+	}
+}
+
+func (this *Scanner) NextShort() Short {
+	rawData := this.Next()
+
+	if data, err := strconv.Atoi(rawData); err != nil {
+		panic(&InputMismatchException{message: rawData})
+	} else {
+		return Short(data)
+	}
+}
+
+func (this *Scanner) NextInt() Integer {
+	rawData := this.Next()
+
+	if data, err := strconv.Atoi(rawData); err != nil {
+		panic(&InputMismatchException{message: rawData})
+	} else {
+		return Integer(data)
+	}
+}
+
+func (this *Scanner) NextLong() Long {
+	rawData := this.Next()
+
+	if data, err := strconv.Atoi(rawData); err != nil {
+		panic(&InputMismatchException{message: rawData})
+	} else {
+		return Long(data)
+	}
+}
+
+func (this *Scanner) NextFloat() Float {
+	rawData := this.Next()
+
+	if data, err := strconv.ParseFloat(rawData, 32); err != nil {
+		panic(&InputMismatchException{message: rawData})
+	} else {
+		return Float(data)
+	}
+}
+
+func (this *Scanner) NextDouble() Double {
+	rawData := this.Next()
+
+	if data, err := strconv.ParseFloat(rawData, 64); err != nil {
+		panic(&InputMismatchException{message: rawData})
+	} else {
+		return Double(data)
+	}
+}
+
+func (this *Scanner) NextBoolean() Boolean {
+	rawData := this.Next()
+
+	if data, err := strconv.ParseBool(rawData); err != nil {
+		panic(&InputMismatchException{message: rawData})
+	} else {
+		return Boolean(data)
 	}
 }
 
