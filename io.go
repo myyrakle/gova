7061d5f78611f16de5cb75748c9ab5e8f43b9975
myyrakle <sssang97@naver.com>
2023-09-27 00:15:11 +0900

    impl: InputStream

--- io.go
+++ io.go
@@ -1,6 +1,9 @@
 package java
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 type PrintStream struct {
 }
@@ -19,4 +22,32 @@ func (p PrintStream) Flush() {
 
 func (p PrintStream) Printf(format string, a ...any) {
 	fmt.Printf(format, a...)
+}
+
+type InputStream struct {
+}
+
+// int read() => Reads the next byte of data from the input stream.
+//
+// int read(byte[] b) => Reads some number of bytes from the input stream and stores them into the buffer array b.
+func (i InputStream) Read(data ...[]byte) int {
+
+	switch len(data) {
+	case 0:
+		buffer := make([]byte, 1)
+
+		if _, err := os.Stdin.Read(buffer); err != nil {
+			return -1
+		} else {
+			return int(buffer[0])
+		}
+	case 1:
+		if data, err := os.Stdin.Read(data[0]); err != nil {
+			return -1
+		} else {
+			return data
+		}
+	default:
+		panic("Too many arguments")
+	}
 }
